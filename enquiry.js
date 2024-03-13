afa1eae074b7cd5f5597e18785fc4d80d6efbd58
Mathews1501 <matthewchipeta@outlook.com>
2024-03-13 14:41:57 +0200

    index add

--- enquiry.js
+++ enquiry.js
@@ -1,15 +1,37 @@
-//alert("The Insurance Broker provides high security for its valued clients click okay to proceed")
+const form = document.querySelector('form');
+const fullName = document.getElementById("fullname");
+const mail = document.getElementById("mail");
+const phone = document.getElementById("phone");
+const message = document.getElementById("msg");
 
 function sendEmail(){
+    const bodyMessage = `Full Name: ${fullName.value}<br> Email: ${mail.value}<br>
+    Contact No.: ${phone.value}<br> Message ${msg.value}`;
+
     Email.send({
         Host : "smtp.gmail.com",
         Username : "buckmarley16@gmail.com",
-        Password : "Suuwhoopbanger/1501",
-        To : "buckmarley16@gmail.com",
-        From : document.getElementById("email").value,
-        Subject : "New Enquiry",
-        Body : "And this is the body"
+        Password : "C49D24A062C589A51389B26262FBBFA0F91D",
+        To : 'buckmarley16@gmail.com',
+        From : document.getElementById("mail").value,
+        Subject : "New enquiry notification",
+        Body : bodyMessage
     }).then(
-      message => alert(message)
+        message => {
+          if (message == "OK")
+              Swal.fire({
+                  title: "Sumbitted!",
+                  text: "Your quote has been sent!",
+                  icon: "success"
+              });
+
+        }
     );
 }
+
+form.addEventListener("submit", (form) => {
+    form.preventDefault();
+
+  sendEmail();
+
+});
