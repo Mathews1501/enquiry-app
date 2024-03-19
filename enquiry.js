bdfca6650e73bb738437381df702b6f760a41f95
Mathews1501 <matthewchipeta@outlook.com>
2024-03-19 13:12:05 +0200

    javascript changes for body message

--- enquiry.js
+++ enquiry.js
@@ -1,6 +1,7 @@
 const fullname = document.getElementById('fullname');
 const mail = document.getElementById('mail');
 const phone = document.getElementById('phone');
+const selectOption = document.getElementById('selectOption');
 const message = document.getElementById('message');
 const submit = document.getElementsByClassName('enquiry-app')[0];
 
@@ -8,16 +9,29 @@ submit.addEventListener('submit',(e)=>{
     e.preventDefault();
     console.log('Clicked');
 
+    let ebody = `
+    <br>Name: <br>${fullname.value}
+    <br>
+    <br>Email: <br>${mail.value}
+    <br>
+    <br>phone: <br>${phone.value}
+    <br>
+    <br>Insurance type: <br>${selectOption.value}
+    <br>
+    <br>${message.value}
+    <br>
+    `
+
     Email.send({
         Host : "smtp.elasticemail.com",
         Username : "mattiesswebdev@gmail.com",
         Password : "54561EB8690093B5D6B0D8DF822D87ABD3A1",
         To : 'mattiesswebdev@gmail.com',
         From : "mattiesswebdev@gmail.com",
-        Subject : "Test email from js",
-        Body : "this is the test email"
+        Subject : "New insurance quote notification",
+        Body : ebody
     }).then(
-      message => alert(message)
+      message => alert("Your message has been sent successfully")
     );
 })
 
