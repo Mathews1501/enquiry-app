7bf3b156815aa0d4e0875bbbea42c9dbe2f1aa8f
Mathews1501 <matthewchipeta@outlook.com>
2024-04-04 13:59:22 +0200

    debug js file

--- enquiry.js
+++ enquiry.js
@@ -1,39 +1,43 @@
-const fullname = document.getElementById('fullname');
-const mail = document.getElementById('mail');
-const phone = document.getElementById('phone');
-const selectOption = document.getElementById('selectOption');
-const message = document.getElementById('message');
-const submit = document.getElementsByClassName('enquiry-app')[0];
+document.addEventListener('DOMContentLoaded', function() {
+  // Your JavaScript code here
+  const fullname = document.getElementById('fullname');
+  const mail = document.getElementById('mail');
+  const phone = document.getElementById('phone');
+  const selectOption = document.getElementById('selectOption');
+  const message = document.getElementById('message');
+  const submit = document.getElementById('submit');
 
-submit.addEventListener('submit',(e)=>{
-    e.preventDefault();
-    console.log('Clicked');
+  submit.addEventListener('click', (e) => {
+      e.preventDefault();
+      console.log('Clicked');
 
-    let ebody = `
-    <br>Name: <br>${fullname.value}
-    <br>
-    <br>Email: <br>${mail.value}
-    <br>
-    <br>phone: <br>${phone.value}
-    <br>
-    <br>Insurance type: <br>${selectOption.value}
-    <br>
-    <br>${message.value}
-    <br>
-    `
+      let ebody = `
+      <br>Name: <br>${fullname.value}
+      <br>
+      <br>Email: <br>${mail.value}
+      <br>
+      <br>phone: <br>${phone.value}
+      <br>
+      <br>Insurance type: <br>${selectOption.value}
+      <br>
+      <br>${message.value}
+      <br>
+      `;
 
-    Email.send({
-        Host : "smtp.elasticemail.com",
-        Username : "mattiesswebdev@gmail.com",
-        Password : "54561EB8690093B5D6B0D8DF822D87ABD3A1",
-        To : 'mattiesswebdev@gmail.com',
-        From : "mattiesswebdev@gmail.com",
-        Subject : "New quotation notication",
-        Body : ebody
-    }).then(
-      message => alert("Your message has been sent successfully")
-    );
-})
+      Email.send({
+          Host : "smtp.elasticemail.com",
+          Username : "mattiesswebdev@gmail.com",
+          Password : "54561EB8690093B5D6B0D8DF822D87ABD3A1",
+          To : 'mattiesswebdev@gmail.com',
+          From : "mattiesswebdev@gmail.com",
+          Subject : "New quotation notification",
+          Body : ebody
+      }).then(
+        message => alert("Quotation sent and recieved successfully")
+      );
+  });
+});
+
 
 // 54561EB8690093B5D6B0D8DF822D87ABD3A1
 
